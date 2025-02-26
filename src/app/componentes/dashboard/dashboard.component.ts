27fb8b43fab72ec746fe9e666ce82da72537b788
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-02-26 16:02:15 -0600

    rutas, login y dashboard

--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { DashComponent } from "../pages/dash/dash.component";
+import { ExperienciasComponent } from "../pages/experiencias/experiencias.component";
+import { EducacionComponent } from "../pages/educacion/educacion.component";
+import { HabilidadesComponent } from "../pages/habilidades/habilidades.component";
+import { CertificadosComponent } from "../pages/certificados/certificados.component";
+import { LogrosComponent } from "../pages/logros/logros.component";
+import { ReferenciasComponent } from "../pages/referencias/referencias.component";
+import { AjustesComponent } from "../pages/ajustes/ajustes.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DashComponent, ExperienciasComponent, EducacionComponent, HabilidadesComponent, CertificadosComponent, LogrosComponent, ReferenciasComponent, AjustesComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  selectedComponent: string = 'Dashboard';
 
+  setSelected(component: string) {
+    this.selectedComponent = component;
+  }
 }
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isFormSubmitted = false;
+  isRightPanelActive: boolean = false;
+  @ViewChild('loginMapContainer', { static: false }) loginMapContainer!: ElementRef;
+  constructor( private router: Router) {}
 
+  username: string = '';
+  email: string = '';
+  password: string = '';
+
+  loginObject = {
+    username: '',
+    password: '',
+    errorMessage: ''
+  }
+
+  togglePanel(activate: boolean): void {
+    this.isRightPanelActive = activate;
+  }
+
+  entrar() {
+    this.router.navigate(['/dashboard']);
+  }
 }
